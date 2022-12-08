d7c7643a1fa1ad745c269ef0d1dbbb586359a199
Strker74 <Strker74@gmail.com>
2022-12-07 20:16:47 -0500

    Linear Slide PID Tuned & Auto StartA

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dreamcode/OpModes/Auto/RedTerminalRedSide.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.dreamcode.OpModes.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous
+public class RedTerminalRedSide extends AutoTemplate {
+
+   @Override
+   public void buildPath() {
+      setStartA(90);
+      path.add(() -> tilePointDrive(0, 1, 0));
+      path.add(() -> tilePointDrive(0, 0, 90));
+      path.add(() -> tilePointDrive(1.5,0 , 90));
+      //path.add(() -> tilePointDrive(2, 2, 90));
+   }
+
+}
